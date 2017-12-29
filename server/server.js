3ed50583ad0038e5082d0685eeb9bf64e0d405b4
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-29 23:33:09 +0530

    Added custom events

--- server/server.js
+++ server/server.js
@@ -14,11 +14,34 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected!");
 
+  // socket.emit('newEmail',{
+  //   from : "sid@sid.com",
+  //   text : "Sup!",
+  //   receivedAt : "hh:mm:ss",
+  // });
+
+  socket.emit('newMessage',{
+    from : "server message",
+    text : "Common message to all my clients",
+    cretaedAt : "Right now ass!"
+  });
+
+  // socket.on('createEmail', (newEmail)=>{
+  //   console.log(`Create Email :- ${JSON.stringify (newEmail,undefined,2)}`);
+  // });
+
+  socket.on('createMessage', (message)=>{
+    console.log(`New message from "${message.from}" with text "${message.text}"`);
+  });
+
   socket.on("disconnect", socket => {
     console.log("User disconnected!");
   });
+
+
 });
 
 server.listen(port, () => {
   console.log(`Server started on PORT ${port}`);
 });
+  
