761583138669a6b2a6c1c9f2e80bc427af9974ef
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-31 00:51:55 +0530

    Added better UI/UX for location messages and mustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -32,24 +32,42 @@ socket.on("disconnect", function() {
 // });
 
 socket.on('newMessage', function (message) {
-
   var formattedTime = moment(message.createdAt).format('h:mm A'); 
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    text : message.text,
+    from : message.from,
+    createdAt : formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
+ 
+  // console.log('newMessage', message);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
   var formattedTime = moment(message.createdAt).format('h:mm A'); 
-  var li = jQuery("<li></li>");
-  var a = jQuery('<a target="_blank">My Current location</a>');
+  // var li = jQuery("<li></li>");
+  // var a = jQuery('<a target="_blank">My Current location</a>');
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href',message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href',message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
+
+  var template =jQuery('#location-template').html();
+  var html = Mustache.render(template,{
+    from : message.from,
+    createdAt : formattedTime,
+    url : message.url
+  });
+
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
