9dc713f6c5a0299ceb5752831f202c190d4770b3
Sai Siddharth Tadepalli <9sidharth96@gmail.com>
2017-12-30 13:15:45 +0530

    Added new user greetings and alert existing users

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,37 @@
 var socket = io();
 
-socket.on('connect', function(){
- console.log("Connected to server");
-    // socket.emit('createEmail', {
-    //     to : "siddharth@gmail.com",
-    //     text : "New email created!",
-    //     createdTime : "some hh:mm:ss"
-    // });
+socket.on("connect", function() {
+  console.log("Connected to server");
 
-    // socket.emit('createMessage',{
-    //     from : "The Client",
-    //     text : "Message from client" 
-    // });
+  //We dont want the event to emit until we're connected hence we
+  //declare it inside the connect callback
+  //  socket.emit('createEmail', {
+  //     from : "Client",
+  //     text : "Sending new email"
+  // });
+
+//   socket.emit("createMessage", {
+//     from: "Siddharth's Phone",
+//     text: "Test from siddharth's phone to server!",
+//     createdAt : new Date().getTime()
+//   });
 });
 
-socket.on('disconnect', function(){
-    console.log("Disconnected to server!");
+socket.on("disconnect", function() {
+  console.log("Disconnected to server!");
 });
 
-// socket.on('newEmail', function (email){
-//     console.log("New email!",email);
-// });
-
-socket.on('newMessage',function(message){
-    console.log(`New Message from "${message.from}" Text = "${message.text}"\n`);
+//Listener for new Event i.e newEmail event, this listenes to the exact
+//same event which gets emitted from server.js
+socket.on("newEmail", function(email) {
+  console.log(JSON.stringify(email, undefined, 2));
 });
+
+socket.on("newMessage", function(message) {
+  console.log(JSON.stringify(message, undefined, 2));
+});
+
+
+socket.on("newMasterMessage", function(message) {
+    console.log(JSON.stringify(message, undefined, 2));
+  });
--- server/server.js
+++ server/server.js
@@ -11,40 +11,58 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on("connection", socket => {
-  console.log("New user connected!");
+io.on("connection", (socket) => {
+  console.log("Connected to client");
 
+  //socket.emit creates the event to a single (only one) connection and sends it off from server to client 
+  //or client to server whereever required. The emit doesent have a callback 
+  //but has data that needs to be emitted from c->s or s->c
   // socket.emit('newEmail',{
-  //   from : "sid@sid.com",
-  //   text : "Sup!",
-  //   receivedAt : "hh:mm:ss",
+  //   from : "Server",
+  //   text : "New Email alert!"
   // });
 
   // socket.emit('newMessage',{
-  //   from : "server message",
-  //   text : "Common message to all my clients",
-  //   cretaedAt : "Right now ass!"
+  //   from : "Siddharth's Server",
+  //   text: "Test message 123 123" 
   // });
+
+  socket.emit('newMessage',{
+    from : "Sabka Baap!",
+    text : "Hello BC!"
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from : "Naya Bandha!",
+    text : "Aaya re apun!"
+  });
+
+  socket.on('createMessage', (newMessage)=>{
+    console.log(JSON.stringify(newMessage,undefined,2));
+    //io.emit creates and sends an event to every connection
+
+    io.emit('newMasterMessage',{
+      from : newMessage.from,
+      text : newMessage.text
+    });
+    
+    //socket,broadcast.emit is a method which sends off the data specified to 
+    //every client except the one which is sending off the message
+ 
+    // socket.broadcast.emit('newMasterMessage',{
+    //   from : newMessage.from,
+    //   text : newMessage.text,
+    //   createdAt : new Date().getTime()
+    // });
+  });
 
   // socket.on('createEmail', (newEmail)=>{
-  //   console.log(`Create Email :- ${JSON.stringify (newEmail,undefined,2)}`);
+  //   console.log(JSON.stringify(newEmail,undefined,2));
   // });
 
-  socket.on('createMessage', (message)=>{
-    console.log(`New message from "${message.from}" with text "${message.text}"`);
-    console.log(message);
-    io.emit('newMessage',{
-      from : message.from,
-      text : message.text,
-      cretaedAt : new Date().getTime()
-    });
+  socket.on("disconnect", (socket) => {
+    console.log("Client disconnected!");
   });
-
-  socket.on("disconnect", socket => {
-    console.log("User disconnected!");
-  });
-
-
 });
 
 server.listen(port, () => {
